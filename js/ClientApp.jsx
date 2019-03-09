b04a286f0054eda6cdb41d9a391bc8d0a2c61e44
Prashant rawal <Prashantrawal619@gmail.com>
2019-03-09 10:42:58 +0530

    added  the data.json and images

---
+++ js/ClientApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let Mycomponent = () => {
+    return (
+        <div>
+            <h1> i am the king</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(Mycomponent,document.getElementById('app'));
