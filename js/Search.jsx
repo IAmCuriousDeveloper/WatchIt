8865dec1206af8a1937871dfca108c01f641ab5f
Prashant rawal <Prashantrawal619@gmail.com>
2019-03-09 20:26:52 +0530

    get the data.json working

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
+import preload from '../data.json' 
 
-export class Search extends Component {
-  render() {
-    return (
-      <div>
-        <h1>this is search</h1>
-      </div>
-    )
-  }
-}
-
-export default Search
+const Search = () => (
+    <div className='search'>
+    {preload.shows.map((show)=>( 
+            <div className="show-card">
+            <img src={`/public/img/posters/${show.poster}`} alt={`${show.title} show poster`}/>
+            <div>
+            <h3>{show.title}</h3>
+            <h4>({show.year})</h4>
+            <p>{show.description}</p>
+            </div>
+            </div>
+    ))}
+    </div>
+        
+)
+export default Search;
