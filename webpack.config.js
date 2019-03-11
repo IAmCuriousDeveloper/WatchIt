b84ff8e8cdd4663307d8b5bc2b878108e1c0c71d
Prashant rawal <Prashantrawal619@gmail.com>
2019-03-11 22:11:58 +0530

    starting hotmodulereplacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     context : __dirname,
-    entry : "./js/ClientApp.jsx",
+    entry : [
+        "react-hot-loader/patch",
+        "webpack-dev-server/client?http://localhost:8080",
+        "webpack/hot/only-dev-server",
+        "./js/ClientApp.jsx"],
     devtool:"cheap-eval-source-map", //its very useful coz whenever we make a mistake it will show us our actual code instead of webpack's bundle code
     output:{
         path:path.join(__dirname,'public'),
@@ -17,10 +22,15 @@ module.exports = {
         chunks:true
     },
     devServer:{
+        hot:true,
         publicPath:'/public',
         historyApiFallback:true
         //what does it mean? if you (server) doesnot know about anything just pass it down to client routing
     },
+    plugins:[
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin()
+    ],
     module:{
         rules:[
             //if you are using eslint we should use below code before babel-loader
