d816ddaad95fd88c6a02d7b0c92ec985ab6f73ab
Prashant rawal <Prashantrawal619@gmail.com>
2020-01-16 20:39:18 +0530

    edits for prod

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 class Landing extends Component {
   render() {
     return (
-    <div className='app'>
-            <div className="landing">
-            <h1>WatchIt</h1>
-            <input type="text" placeholder='Search'/>
-            <Link to='/search'>or Browse all</Link>
-            </div>
+      <div className="app">
+        <div className="landing">
+          <h1>WatchIt</h1>
+          <input type="text" placeholder="please go to browse then search" />
+          <Link to="/search">or Browse all</Link>
         </div>
-    )
+      </div>
+    );
   }
 }
 
-export default Landing
+export default Landing;
